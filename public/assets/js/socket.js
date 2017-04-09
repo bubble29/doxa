f4aeb2dbe022ee2b680843d3d75fa0c68471cbdd
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2017-04-08 21:06:27 -0400

    Added newMessage and createMessage

--- public/assets/js/socket.js
+++ public/assets/js/socket.js
@@ -1,9 +1,18 @@
 var socket = io();
 
-socket.on('connect', () => {
+socket.on('connect', function() {
   console.log("Connected to server");
+
+  socket.emit('createMessage', {
+    from: 'Reni',
+    text: 'Hey Hey'
+  });
 });
 
-socket.on('disconnect', () => {
+socket.on('disconnect', function() {
   console.log("Disconnected from server");
+});
+
+socket.on('newMessage', function(message) {
+  console.log("New Message", message);
 });
