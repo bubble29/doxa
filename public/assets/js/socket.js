c75d11ec3387c5489551346f33bc1b1e556cbca9
Reynaldo Salcedo <salcedoreynaldo5@gmail.com>
2017-04-08 21:19:15 -0400

    Emit newMessage on createMessage

--- public/assets/js/socket.js
+++ public/assets/js/socket.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log("Connected to server");
-
-  socket.emit('createMessage', {
-    from: 'Reni',
-    text: 'Hey Hey'
-  });
 });
 
 socket.on('disconnect', function() {
